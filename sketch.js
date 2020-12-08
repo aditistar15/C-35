bb3a4751685cf1c6dc4eb0bece5ec0adb50aa5d1
aditistar15 <aditistar15@gmail.com>
2020-12-08 17:20:30 +0530

    database

--- sketch.js
+++ sketch.js
@@ -1,24 +1,55 @@
+var database, position;
+
+
 var ball;
 
+
+
 function setup(){
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
+    createCanvas(400,400);
+    ball = createSprite(10,10,10,10);
     ball.shapeColor = "red";
+    // .ref() ======== location in database
+    //.on() ========== read from the dB
+    //.set() ========= write into dB
+
+    database = firebase.database(); 
+    position = database.ref("ball/position");
+    position.on("value", readPosition, showError);
+}
+
+function readPosition(data){
+    ballPosition = data.val();
+    ball.x = ballPosition.x;
+    ball.y =ballPosition.y; 
+
+}
+
+function showError(){
+    console.log("sorry, some error !!")
+}
+
+function writePosition(x,y){
+  database.ref ("ball/position").set({
+      x:ball.x+x,
+      y:ball.y+y
+  })
+
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
